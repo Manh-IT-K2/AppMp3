d1620dad8ef52aecbc8f8546df4ad13e4ecd6a2c
Mạnh <100762257+Qmanh1804@users.noreply.github.com>
2024-02-22 21:29:32 +0700

    fix library

---
+++ app/src/main/java/com/example/app/Media/ExoPlayer/MediaPlayerNotificationManager.kt
@@ -0,0 +1,72 @@
+package com.example.app.Media.ExoPlayer
+
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import com.example.app.Media.Constants.K
+import com.example.app.R
+import com.google.android.exoplayer2.Player
+
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+
+internal class MediaPlayerNotificationManager(
+    context : Context,
+    sessionToken : MediaSessionCompat.Token,
+    notificationListener : PlayerNotificationManager.NotificationListener
+) {
+    private val notificationManager : PlayerNotificationManager
+
+    init {
+        val mediaController = MediaControllerCompat(context,sessionToken)
+        val builder = PlayerNotificationManager.Builder(
+            context,
+            K.PLAYBACK_NOTIFICATION_ID.toInt(),
+            K.PLAYBACK_NOTIFICATION_CHANNEL_ID
+        )
+
+        with(builder){
+            setMediaDescriptionAdapter(DescriptionAdapter(mediaController))
+            setNotificationListener(notificationListener)
+            setChannelNameResourceId(R.string.app_name)
+            setChannelDescriptionResourceId(R.string.app_name)
+        }
+
+        notificationManager = builder.build()
+        with(notificationManager){
+            setMediaSessionToken(sessionToken)
+            setSmallIcon(R.drawable.baseline_music_note_24)
+            setUseRewindAction(false)
+            setUseFastForwardAction(false)
+        }
+
+        fun hideNotification(){
+            notificationManager.setPlayer(null)
+        }
+
+        fun showNotification(player : Player){
+            notificationManager.setPlayer(player)
+        }
+    }
+
+    inner class DescriptionAdapter(private val controller: MediaControllerCompat):
+            PlayerNotificationManager.MediaDescriptionAdapter{
+        override fun getCurrentContentTitle(player: Player): CharSequence {
+            TODO("Not yet implemented")
+        }
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? =
+            controller.sessionActivity
+
+        override fun getCurrentContentText(player: Player): CharSequence? =
+            controller.metadata.description.subtitle
+
+        override fun getCurrentLargeIcon(
+            player: Player,
+            callback: PlayerNotificationManager.BitmapCallback
+        ): Bitmap? {
+           return null
+        }
+    }
+}
--- app/src/main/java/com/example/app/Media/Service/MediaPlayerService.kt
+++ app/src/main/java/com/example/app/Media/Service/MediaPlayerService.kt
@@ -2,12 +2,17 @@ package com.example.app.Media.Service
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.example.app.Media.Constants.K
 import com.example.app.Media.ExoPlayer.MediaSource
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -20,6 +25,12 @@ class MediaPlayerService: MediaBrowserServiceCompat() {
 
     @Inject
     lateinit var mediaSource: MediaSource
+
+    private val serviceJob = SupervisorJob()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession : MediaSessionCompat
+    private lateinit var mediaSessionConnector : MediaSessionConnector
 
     override fun onGetRoot(
         clientPackageName: String,
